c632e0a31417480b3e30e50f66b7a8b263ee3967
Sanjay Sachdev <89441239+sasachde@users.noreply.github.com>
2023-08-02 10:42:24 +0530

    Update train.py to include autologging and a function to split the input dataset into training and test sets

--- src/model/train.py
+++ src/model/train.py
@@ -4,15 +4,18 @@ import argparse
 import glob
 import os
 
+import mlflow
+
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,6 +37,15 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    # split the dataset into features and label
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+
+    # split the features and label into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+    # return the training and test sets
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
